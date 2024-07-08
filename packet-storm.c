d6f4436d3468bf695c7b902a622ddb55b5163c15
alimmeek <69384169+alimmeek@users.noreply.github.com>
2024-07-08 22:54:53 +0100

    dump packets

--- packet-storm.c
+++ packet-storm.c
@@ -11,7 +11,58 @@ TODO:
         - Default is just a .txt file to make sharing results easier
 */
 
-void packet_handler(struct pcap_pkthdr *header, const unsigned char *packet);
+void packet_handler(struct pcap_pkthdr *header, const unsigned char *packet) {
+    unsigned int i;
+    int length = header->len;
+    // Decode Packet Header
+    struct ether_header *eth_header = (struct ether_header *) data;
+    printf("\n\n === PACKET %ld HEADER ===", pcount);
+    printf("\nSource MAC: ");
+    for (i = 0; i < 6; ++i) {
+    printf("%02x", eth_header->ether_shost[i]);
+    if (i < 5) {
+        printf(":");
+    }
+    }
+    printf("\nDestination MAC: ");
+    for (i = 0; i < 6; ++i) {
+    printf("%02x", eth_header->ether_dhost[i]);
+    if (i < 5) {
+        printf(":");
+    }
+    }
+    printf("\nType: %hu\n", eth_header->ether_type);
+    printf(" === PACKET %ld DATA == \n", pcount);
+    // Decode Packet Data (Skipping over the header)
+    int data_bytes = length - ETH_HLEN;
+    const unsigned char *payload = data + ETH_HLEN;
+    const static int output_sz = 20; // Output this many bytes at a time
+    while (data_bytes > 0) {
+        int output_bytes = data_bytes < output_sz ? data_bytes : output_sz;
+        // Print data in raw hexadecimal form
+        for (i = 0; i < output_sz; ++i) {
+            if (i < output_bytes) {
+                printf("%02x ", payload[i]);
+            } else {
+                printf ("   "); // Maintain padding for partial lines
+            }
+        }
+        printf ("| ");
+        // Print data in ascii form
+        for (i = 0; i < output_bytes; ++i) {
+            char byte = payload[i];
+            if (byte > 31 && byte < 127) {
+            // Byte is in printable ascii range
+                printf("%c", byte);
+            } else {
+                printf(".");
+            }
+        }
+        printf("\n");
+        payload += output_bytes;
+        data_bytes -= output_bytes;
+    }
+}
 
 void packet_init(unsigned char * args, struct pcap_pkthdr *header, const unsigned char *packet) {
     pcap_pkthdr pkt_header;
@@ -29,7 +80,7 @@ int main() {
     pcap_t *handle = pcap_open_offline("packet-storm.pcap", error_buffer);
 
     // "A value of -1 or 0 for cnt causes all the packets received in one buffer to be processed when reading a live capture and causes all the packets in a file to be processed when reading a savefile"
-    pcap_loop(handle, -1, (void*) &handler, NULL);
+    pcap_loop(handle, -1, (void*) &packet_handler, NULL);
 
-    return;
+    return 0;
 }
