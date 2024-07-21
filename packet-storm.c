15e258d8aae155d46849358833d2287f5997795d
Alia Meek <alimmeek12@gmail.com>
2024-07-21 16:15:09 +0100

    Multithreading done!

--- packet-storm.c
+++ packet-storm.c
@@ -6,11 +6,14 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <string.h>
+#include <pthread.h>
 #include <inttypes.h>
+#include <time.h>
 
 #define TCP 6
 #define UDP 17
 #define UDP_HEADER_LENGTH 8
+#define MAX_THREADS 8
 
 /*
 TODO:
@@ -37,6 +40,11 @@ typedef struct list_node {
     node_t node;
 } list_t;
 
+typedef struct Param {
+  struct pcap_pkthdr *header;
+  const unsigned char *packet;
+} Param;
+
 
 list_t *ips_head = NULL;
 int unique_ips = 0;
@@ -45,6 +53,20 @@ int udp_count = 0;
 int packets_count = 0;
 int average_size = 0;
 int total_payload = 0;
+
+int available_indices[MAX_THREADS];   // queue of threads not doing anything
+int thread_count = MAX_THREADS;       // number of threads available
+pthread_t threads[MAX_THREADS];       // threads
+pthread_mutex_t threads_mutex = PTHREAD_MUTEX_INITIALIZER;    // mutex lock for available_indices
+pthread_cond_t no_threads_cond = PTHREAD_COND_INITIALIZER;    // signals a thread is available
+pthread_mutex_t ip_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void mthread_setup() {  // initialises available_indices to store its current index
+  int i;
+  for(i=0; i<MAX_THREADS; i++) {
+    available_indices[i] = i;
+  }
+}
 
 list_t *SortedMerge(list_t *a, list_t *b);
 
@@ -124,7 +146,12 @@ void FrontBackSplit(list_t *source, list_t **frontRef, list_t **backRef)
 }
 
 
-void packet_handler(struct pcap_pkthdr *header, const unsigned char *packet) {
+void *packet_handler(void *_i) {
+
+    Param a = *((Param *) _i);
+
+    struct pcap_pkthdr *header = a.header;
+    const unsigned char *packet = a.packet;
 
     average_size += header->len;
 
@@ -133,6 +160,7 @@ void packet_handler(struct pcap_pkthdr *header, const unsigned char *packet) {
     if (ntohs(eth_header->ether_type) == ETH_P_IP) {
         struct ip *ip_hdr = (struct ip *) (packet + ETH_HLEN);
 
+        pthread_mutex_lock(&ip_list_mutex);
         list_t **head_pointer = &ips_head;
 
         if (unique_ips == 0) {            
@@ -181,6 +209,7 @@ void packet_handler(struct pcap_pkthdr *header, const unsigned char *packet) {
                 unique_ips++;
             }
         }
+        pthread_mutex_unlock(&ip_list_mutex);
 
         unsigned int size_ip = ip_hdr->ip_hl;
 
@@ -202,7 +231,38 @@ void packet_init(unsigned char * args, struct pcap_pkthdr *header, const unsigne
     memcpy((void *) &pkt_header, &header, sizeof(header));
     memcpy((void *) &pkt_packet, &packet, sizeof(packet));
 
-    packet_handler(pkt_header, pkt_packet);
+    Param a_p = {pkt_header, pkt_packet};
+
+    // if threads are available, acquire mutex lock for available_threads, otherwise wait
+    if (thread_count > 0) {
+        pthread_mutex_lock(&threads_mutex);
+    } else {
+        while (thread_count == 0) {
+        pthread_cond_wait(&no_threads_cond, &threads_mutex);
+        }
+    }
+
+    // take the first available index, then shuffle the remaining indices forward one position
+    // decrement the number of available threads and release the mutex lock
+    int i;
+    int t = available_indices[0];
+    for (i=0; i < thread_count-1; i++) {
+        available_indices[i] = available_indices[i+1];
+    }
+    thread_count--;
+    pthread_mutex_unlock(&threads_mutex);
+
+    // create the thread and join it back to the main program
+    pthread_create(&threads[t], NULL, &packet_handler, &a_p);
+    pthread_join(threads[t], NULL);
+
+    // acquire mutex lock, and add the index to the back of the queue
+    // incremenet the number of available threads and signal anything waiting for a thread
+    pthread_mutex_lock(&threads_mutex);
+    available_indices[thread_count-1] = t;
+    thread_count++;
+    pthread_cond_signal(&no_threads_cond);
+    pthread_mutex_unlock(&threads_mutex);
 
     packets_count++;
     if (packets_count % 100000 == 0) {
@@ -224,7 +284,13 @@ void print_ips() {
 
 
 int main() {
+
+    clock_t start, stop;
     char error_buffer[PCAP_ERRBUF_SIZE];
+
+    start = clock();
+
+    mthread_setup();
     
     pcap_t *handle = pcap_open_offline("packet-storm.pcap", error_buffer);
 
@@ -240,6 +306,10 @@ int main() {
     printf("Number of packets sent with UDP: %d\n", udp_count);
     printf("Total payload: %d bytes\n", total_payload);
     printf("Average size of packets received: %d bytes\n", average_size);
+
+    stop = clock();
+
+    printf("Program run time: %f seconds \n", (double) (stop - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
